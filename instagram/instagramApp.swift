75f4df360b91d915b9a4b316c175873c3a9ab172
Miguel Angel <gutierrezmayamiguelangel@gmail.com>
2024-07-28 15:57:21 -0500

    Initial Commit

---
+++ instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by Miguel Angel Gutierrez Maya on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
